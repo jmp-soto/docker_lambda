ffe581ca354c627f49b906b39fb5d17048d392a3
jose maria <em.vega.minaya@gmail.com>
2026-01-25 00:37:02 -0500

    agregar primer despliegue

--- scripts/deploy-from-title.sh
+++ scripts/deploy-from-title.sh
@@ -7,7 +7,7 @@ LAMBDA_NAME=$(echo "$TITLE" | cut -d':' -f2 | xargs)  # Added xargs to trim whit
 TEMPLATE="infrastructure/${LAMBDA_NAME}.yml"
 
 echo "Current directory: $(pwd)"
-echo "Deploying Lambda Image: $LAMBDA_NAME"
+echo " Deploying Lambda Image: $LAMBDA_NAME"
 echo "Using template: $TEMPLATE"
 echo "Template exists: $(if [ -f "$TEMPLATE" ]; then echo "YES"; else echo "NO"; fi)"
 
